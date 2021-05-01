ba97421d836fa349418c104c66b21c1b8907dd0a
Wade028 <46267078+Wade028@users.noreply.github.com>
2021-05-01 23:21:55 +0200

    Step Zero

---
+++ Assets/MyAssets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 1;
+    void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * moveSpeed, moveVertical * moveSpeed, 0);
+            transform.position += movement;
+        }
+    }
+
+    void Update() 
+    {
+        HandleMovement();
+    }
+
+}
